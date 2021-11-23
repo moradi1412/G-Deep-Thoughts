ff58b6eed14fe2e1ab6cddf3e0120d47d6007a7a
Moradi1412 <moradi.javid@gmail.com>
2021-11-23 11:59:15 -0800

    compare codes

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHT } from '../utils/queries';
+
 import ReactionList from '../components/ReactionList';
 
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_THOUGHT } from '../utils/queries';
 
 const SingleThought = props => {
-
   const { id: thoughtId } = useParams();
 
   const { loading, data } = useQuery(QUERY_THOUGHT, {
@@ -21,20 +21,20 @@ const SingleThought = props => {
 
   return (
     <div>
-    <div className="card mb-3">
-      <p className="card-header">
-        <span style={{ fontWeight: 700 }} className="text-light">
-          {thought.username}
-        </span>{' '}
-        thought on {thought.createdAt}
-      </p>
-      <div className="card-body">
-        <p>{thought.thoughtText}</p>
+      <div className="card mb-3">
+        <p className="card-header">
+          <span style={{ fontWeight: 700 }} className="text-light">
+            {thought.username}
+          </span>{' '}
+          thought on {thought.createdAt}
+        </p>
+        <div className="card-body">
+          <p>{thought.thoughtText}</p>
+        </div>
       </div>
+
+      {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
     </div>
-  
-    {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
-  </div>
   );
 };
 
